8d6798860294f993b672183cc796813a7c728ca2
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 13:13:00 +0530

    IMG

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 const FirstPost = () => {
   return (
     <div>
         <h1>first-post</h1>
+        <Image src="/../public/images/profile.jpg" width={150} height={150} alt="Bhavya"/>
         <Link href='/'>to home</Link>
     </div>
   )
