accb160f4b55edb9260012d13a29023e80ddf509
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 12:51:02 +0530

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+const FirstPost = () => {
+  return (
+    <div>
+        <h1>first-post</h1>
+        <Link href='/'>to home</Link>
+    </div>
+  )
+}
+
+export default FirstPost
