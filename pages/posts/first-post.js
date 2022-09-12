4aa78ca009e68aaf47cea3613036438c2dd13e49
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 13:30:24 +0530

    css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,24 +3,21 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 import Script from 'next/script'
+import layout from '../components/layout'
+
 const FirstPost = () => {
   return (
-    <div>
-      <Head>
-        <title>BLog</title>
-      </Head>
-      <Script
-      src='https://connect.facebook.net/en_US/sdk.js'
-      strategy='lazyOnLoad'
-      onLoad={() =>
-        console.log(`script loaded correctly, window.FB has been populated`)
-      }
-      />
-
-        <h1>first-post</h1>
-        <Image src="/../public/images/profile.jpg" width={150} height={150} alt="Bhavya"/>
-        <Link href='/'>to home</Link>
-    </div>
+    <layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h2>
+  </layout>
   )
 }
 
